e4307264c1a35fd00c21e0506c7c8228c983c4b1
leonardecavele <leonard.decavele@gmail.com>
2026-03-30 19:48:50 +0200

    edit: usage of multiple inputs

--- srcs/kmain.c
+++ srcs/kmain.c
@@ -4,6 +4,9 @@
 #include "irq.h"
 
 static volatile bool g_int23_button = false;
+static volatile bool g_int24_button = false;
+static volatile bool g_int25_button = false;
+static volatile bool g_int26_button = false;
 
 void handle_irq(void)
 {
@@ -11,6 +14,21 @@ void handle_irq(void)
 	{
 		gpio_event_clear(23u);
 		g_int23_button = true;
+	}
+	else if (gpio_event_pending(24u))
+	{
+		gpio_event_clear(24u);
+		g_int24_button = true;
+	}
+	else if (gpio_event_pending(25u))
+	{
+		gpio_event_clear(25u);
+		g_int25_button = true;
+	}
+	else if (gpio_event_pending(26u))
+	{
+		gpio_event_clear(26u);
+		g_int26_button = true;
 	}
 }
 
@@ -39,12 +57,33 @@ int kmain(uintptr_t dtb)
 	uart_printf(BCM2835_UART0, "hello world\r\n");
 	//wait_cmd(BCM2835_UART0);
 
-	/* set-up button GPIO */
+	/* set-up button GPIO 23 */
 	gpio_set_func(23u, GPIO_INPUT);
 	gpio_set_pull(23u, GPIO_PULL_UP);
 	gpio_event_clear(23u);
 	gpio_enable_falling(23u);
 	gpio_event_clear(23u);
+
+	/* set-up button GPIO 24 */
+	gpio_set_func(24u, GPIO_INPUT);
+	gpio_set_pull(24u, GPIO_PULL_UP);
+	gpio_event_clear(24u);
+	gpio_enable_falling(24u);
+	gpio_event_clear(24u);
+
+	/* set-up button GPIO 25 */
+	gpio_set_func(25u, GPIO_INPUT);
+	gpio_set_pull(25u, GPIO_PULL_UP);
+	gpio_event_clear(25u);
+	gpio_enable_falling(25u);
+	gpio_event_clear(25u);
+
+	/* set-up button GPIO 26 */
+	gpio_set_func(26u, GPIO_INPUT);
+	gpio_set_pull(26u, GPIO_PULL_UP);
+	gpio_event_clear(26u);
+	gpio_enable_falling(26u);
+	gpio_event_clear(26u);
 
 	irq_controller_reset();
 	irq_controller_enable(IRQ_GPIO_BANK0);
@@ -58,9 +97,27 @@ int kmain(uintptr_t dtb)
 			g_int23_button = false;
 			led_state = !led_state;
 			gpio_write(17u, led_state);
+		}
+		else if (g_int24_button)
+		{
+			g_int24_button = false;
+			led_state = !led_state;
+			gpio_write(27u, led_state);
+		}
+		else if (g_int25_button)
+		{
+			g_int25_button = false;
+			led_state = !led_state;
+			gpio_write(22u, led_state);
+		}
+		else if (g_int26_button)
+		{
+			g_int26_button = false;
+			led_state = !led_state;
+			gpio_write(17u, led_state);
 			gpio_write(27u, led_state);
 			gpio_write(22u, led_state);
-			uart_printf(BCM2835_UART0, "interrupt 23\r\n");
 		}
+		sleep();
 	}
 }
