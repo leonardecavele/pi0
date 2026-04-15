cfcb53f0390453fb8e728e8e2cee5d7fe425a2af
leonardecavele <leonard.decavele@gmail.com>
2026-04-15 19:53:33 +0200

    wip

--- includes/display.h
+++ includes/display.h
@@ -11,7 +11,51 @@ typedef enum e_display_mode
 	DISPLAY_BYTES = 1
 }	t_display_mode;
 
-void display_reset(void);
-void display_set_mode(t_display_mode mode);
+typedef enum e_display_cmd
+{
+	DISPLAY_CMD_NOP      = 0x00, /* do nothing */
+	DISPLAY_CMD_SWRESET  = 0x01, /* software reset */
+	DISPLAY_CMD_RDDID    = 0x04, /* read display ID */
+	DISPLAY_CMD_RDDST    = 0x09, /* read display status */
+
+	DISPLAY_CMD_SLPIN    = 0x10, /* enter sleep mode */
+	DISPLAY_CMD_SLPOUT   = 0x11, /* leave sleep mode */
+	DISPLAY_CMD_PTLON    = 0x12, /* enable partial mode */
+	DISPLAY_CMD_NORON    = 0x13, /* return to normal display mode */
+
+	DISPLAY_CMD_INVOFF   = 0x20, /* disable color inversion */
+	DISPLAY_CMD_INVON    = 0x21, /* enable color inversion */
+	DISPLAY_CMD_GAMSET   = 0x26, /* set gamma curve */
+	DISPLAY_CMD_DISPOFF  = 0x28, /* turn display off */
+	DISPLAY_CMD_DISPON   = 0x29, /* turn display on */
+
+	DISPLAY_CMD_CASET    = 0x2A, /* set column range */
+	DISPLAY_CMD_RASET    = 0x2B, /* set row range */
+	DISPLAY_CMD_RAMWR    = 0x2C, /* write pixels to display RAM */
+	DISPLAY_CMD_RAMRD    = 0x2E, /* read pixels from display RAM */
+
+	DISPLAY_CMD_PTLAR    = 0x30, /* set partial area */
+	DISPLAY_CMD_MADCTL   = 0x36, /* set orientation and RGB/BGR order */
+	DISPLAY_CMD_COLMOD   = 0x3A, /* set pixel format */
+
+	DISPLAY_CMD_FRMCTR1  = 0xB1, /* frame rate control 1 */
+	DISPLAY_CMD_FRMCTR2  = 0xB2, /* frame rate control 2 */
+	DISPLAY_CMD_FRMCTR3  = 0xB3, /* frame rate control 3 */
+	DISPLAY_CMD_INVCTR   = 0xB4, /* inversion control */
+	DISPLAY_CMD_DISSET5  = 0xB6, /* display function control */
+
+	DISPLAY_CMD_PWCTR1   = 0xC0, /* power control 1 */
+	DISPLAY_CMD_PWCTR2   = 0xC1, /* power control 2 */
+	DISPLAY_CMD_PWCTR3   = 0xC2, /* power control 3 */
+	DISPLAY_CMD_PWCTR4   = 0xC3, /* power control 4 */
+	DISPLAY_CMD_PWCTR5   = 0xC4, /* power control 5 */
+	DISPLAY_CMD_VMCTR1   = 0xC5, /* VCOM control 1 */
+
+	DISPLAY_CMD_GMCTRP1  = 0xE0, /* positive gamma correction */
+	DISPLAY_CMD_GMCTRN1  = 0xE1  /* negative gamma correction */
+}	t_display_cmd;
+
+void	display_reset(void);
+void	display_set_mode(t_display_mode mode);
 
 #endif
--- srcs/display.c
+++ srcs/display.c
@@ -19,3 +19,91 @@ void display_set_mode(t_display_mode mode)
 	else
 		gpio_write(DISPLAY_DC_GPIO, true);
 }
+
+/* build a structure screen with height and size so functions can
+   adapt by itself according to the screen
+*/
+
+/* to do */
+void cursed_square(void)
+{
+	spi_init(64u, SPI_MODE0);
+	display_reset();
+
+	display_set_mode(DISPLAY_COMMAND);
+	spi_begin();
+	spi_write(0x01);
+	spi_end();
+
+	display_set_mode(DISPLAY_COMMAND);
+	spi_begin();
+	spi_write(0x11);
+	spi_end();
+	usleep(150000u);
+
+	display_set_mode(DISPLAY_COMMAND);
+	spi_begin();
+	spi_write(0x3A);
+	spi_end();
+
+	display_set_mode(DISPLAY_BYTES);
+	spi_begin();
+	spi_write(0x05);
+	spi_end();
+
+	display_set_mode(DISPLAY_COMMAND);
+	spi_begin();
+	spi_write(0x36);
+	spi_end();
+
+	display_set_mode(DISPLAY_BYTES);
+	spi_begin();
+	spi_write(0x00);
+	spi_end();
+
+	display_set_mode(DISPLAY_COMMAND);
+	spi_begin();
+	spi_write(0x29);
+	spi_end();
+	usleep(20000u);
+
+	display_set_mode(DISPLAY_COMMAND);
+	spi_begin();
+	spi_write(0x2A);
+	spi_end();
+
+	display_set_mode(DISPLAY_BYTES);
+	spi_begin();
+	spi_write(0x00);
+	spi_write(0x00);
+	spi_write(0x00);
+	spi_write(127);
+	spi_end();
+
+	display_set_mode(DISPLAY_COMMAND);
+	spi_begin();
+	spi_write(0x2B);
+	spi_end();
+
+	display_set_mode(DISPLAY_BYTES);
+	spi_begin();
+	spi_write(0x00);
+	spi_write(0x00);
+	spi_write(0x00);
+	spi_write(127);
+	spi_end();
+
+	display_set_mode(DISPLAY_COMMAND);
+	spi_begin();
+	spi_write(0x2C);
+	spi_end();
+
+	display_set_mode(DISPLAY_BYTES);
+	spi_begin();
+	for (uint32_t i = 0; i < 128u * 128u; ++i)
+	{
+		spi_write(0xF8);
+		spi_write(0x00);
+	}
+	spi_end();
+}
